bc250656082acbf961eb6e07e9d30ccfca2bbd67
David Boissonnade <davidboissonnade@gmail.com>
2021-01-11 16:51:58 +0100

    Controller

---
+++ AveyRond/src/Controller/AveyrondController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AveyrondController extends AbstractController
+{
+    /**
+     * @Route("/aveyrond", name="aveyrond")
+     */
+    public function index(): Response
+    {
+        return $this->render('aveyrond/index.html.twig', [
+            'controller_name' => 'AveyrondController',
+        ]);
+    }
+}
